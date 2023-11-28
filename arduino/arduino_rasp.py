b1417dbabe108c9ac678272f581b2deb9f67858d
ikk5515 <ikk5515@naver.com>
2023-11-28 11:31:16 +0900

    arduino 연동 코드에러 수정

--- arduino/arduino_rasp.py
+++ arduino/arduino_rasp.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(file))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import time
 import serial
