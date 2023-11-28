5c252c5ae8a3156019eda5a2bbf0593a469da964
ebcsrh <113304586+ebcsrh@users.noreply.github.com>
2023-11-28 12:28:46 +0900

    아두이노 코드 수정-
    아래 라즈베리파이 코드와 함께, 아두이노에서 센서 2가지 연결해서 측정한 값이 동시에 라즈베리파이로 넘어가는 것 확인했어요!

--- arduino/ard_sensor.ino
+++ arduino/ard_sensor.ino
@@ -1,12 +1,14 @@
 //#define DHTPIN @@@  // 온습도 핀 번호
 //#define DHTTYPE @@@    // 센서 (DHT11)
 #define MQ5pin 3 // 가스센서 핀 번호
+#define MQ7pin 5 // 가스센서 핀 번호
 
 
 //DHT dht(DHTPIN, DHTTYPE)
 
 int chk;
-float gas;    // 가스 센서
+float mq5;    // 가스 센서
+float mq7;    // 가스 센서
 float hum;    // 습도
 float temp;   // 온도
 
@@ -30,10 +32,13 @@ void loop() {
   Serial.print("온도 인식값 : ");  
   Serial.println("temp");  */
 
-  gas = analogRead(MQ5pin);       // 가스센서 인식값
+  mq5 = analogRead(MQ5pin);       // 가스센서 인식값
+  mq7 = analogRead(MQ7pin); 
 
-  Serial.print("가스센서 인식값 : ");   
-  Serial.println(gas);
+  Serial.print("mq5:");   
+  Serial.println(mq5);
+  Serial.print("mq7:");   
+  Serial.println(mq7);
 
   // 시리얼모니터의 값을 라즈베리파이에서 줄별로 읽고 센서 종류를 구분하여 인식값을 받아 웹소켓으로 전달 -
 
