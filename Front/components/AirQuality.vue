8a73eb53f386905234cc8082fcab532f2d3cdabc
최지혁 <113498312+choiji12@users.noreply.github.com>
2023-12-03 00:04:27 +0900

    Design

--- Front/components/AirQuality.vue
+++ Front/components/AirQuality.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
-    <h1>센서 값</h1>
+    <img src="@/assets/inhatc.png" alt="로고" class="logo" />
+    <!--<h1>센서 값</h1>-->
     <div class="input-group">
       <select v-model="selectedSensor" class="select-sensor">
         <option v-for="sensor in sensorList" :key="sensor" :value="sensor">{{ sensor }}</option>
-        <!-- 다른 센서 옵션을 추가! -->
+        <!-- 다른 센서 옵션을 추가 -->
       </select>
       <button @click="getData" class="get-data-button">Get Data</button>
     </div>
@@ -14,9 +15,9 @@
 
     <!-- 싸이클 POST -->
     <div class="input-group">
-      <label for="cycleValue">Cycle Value:</label>
+      <label for="cycleValue">측정주기:</label>
       <input type="number" v-model="cycleValue" id="cycleValue" />
-      <button @click="changeCycle" class="change-cycle-button">Change Cycle</button>
+      <button @click="changeCycle" class="change-cycle-button">변경</button>
     </div>
   </div>
 </template>
@@ -49,9 +50,19 @@ export default {
         const response = await axios.get(`https://port-0-raspberry-pi-project-5mk12alpbcv53c.sel5.cloudtype.app/sensors/graph/${this.selectedSensor}`);
         this.responseData = response.data;
         this.renderChart(); // 데이터를 가져온 후 차트를 렌더링
+        this.calculateAverage();
       } catch (error) {
         console.error('Error fetching data:', error);
       }
+    },
+
+    calculateAverage() {
+      const values = this.responseData.map(item => item.measure_value);
+      const sum = values.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      const average = sum / values.length;
+      const roundedAverage = average.toFixed(0); // 평균 값을 1의 자리로 반올림
+      console.log(`선택된 센서에 대한 값의 평균: ${roundedAverage}`);
+      this.averageValue = roundedAverage;
     },
     renderChart() {
       if (this.chart) {
@@ -119,13 +130,14 @@ export default {
 <style>
 .container {
   max-width: 800px;
-  margin: 0 auto;
+  margin: 140px auto 0; /* 상단 여백을 조정하여 컨테이너를 상단에서 20px 떨어지게 설정 */
   padding: 20px;
   font-family: 'Arial', sans-serif;
-  background-color: #f9f9f9;
+  background-color: #f5f5f5;
   border-radius: 8px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
+
 
 h1 {
   text-align: center;
@@ -136,21 +148,35 @@ h1 {
 .input-group {
   display: flex;
   align-items: center;
-  margin-bottom: 20px;
+  justify-content: flex-end;
 }
 
+.label {
+  margin-right: 10px; /* 라벨과 인풋 사이의 간격 조절 */
+}
+#cycleValue {
+  text-align: right;
+}
 .select-sensor {
-  flex: 1;
-  padding: 10px;
-  font-size: 16px;
+  flex: 0 0 auto; /* 유연한 크기 조정을 막고 고정 크기로 설정 */
+  width: 150px; /* 원하는 크기로 설정 */
+  padding: 8px; /* 적절한 패딩 적용 */
+  font-size: 14px;
   border: 1px solid #ccc;
   border-radius: 4px;
+  margin-left: auto; /* 오른쪽으로 배치 */
+  margin-right: 10px; /* 오른쪽 여백 설정 */
   outline: none;
 }
-
-.get-data-button {
+.logo {
+  width: 200px; /* 로고 이미지의 너비 설정 */
+  height: auto; /* 높이 자동 조정 */
+  position: absolute; /* 위치를 설정하기 위해 절대 위치 지정 */
+  top: 30px; /* 컨테이너 상단에서의 위치 조정 */
+  left: 30px; /* 컨테이너 왼쪽에서의 위치 조정 */
+}
+.get-data-button, .change-cycle-button {
   padding: 10px 20px;
-  margin-left: 10px;
   border: none;
   border-radius: 4px;
   background-color: #4caf50;
@@ -159,7 +185,7 @@ h1 {
   transition: background-color 0.3s;
 }
 
-.get-data-button:hover {
+.get-data-button:hover, .change-cycle-button {
   background-color: #45a049;
 }
 
@@ -168,10 +194,37 @@ h1 {
   padding: 20px;
   border-radius: 4px;
   background-color: #fff;
+  display: flex; /* 부모 요소를 플렉스로 설정하여 자식 요소를 정렬 */
+  justify-content: center; /* 가로 방향 가운데 정렬 */
+  align-items: center; /* 세로 방향 가운데 정렬 */
+  margin-top: 20px; /* 상단 여백을 추가하여 차트를 조금 더 아래쪽에 배치 */
 }
 .chart-canvas {
   width: 100%; /* 차트 요소를 부모 요소의 100%로 설정 */
   height: 400px; /* 원하는 높이로 설정하세요 */
+}
+.cycle-input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.cycle-input label {
+  margin-right: 10px;
+}
+
+#cycleValue {
+  padding: 8px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100px;
+  margin-right: 10px;
+}
+
+.change-cycle-button {
+  padding: 10px 18px;
+  background-color: #4285f4;
 }
 /* ... */
 </style>
